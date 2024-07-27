0d728a91c4ff7acaa24cab71955c7e816e42975a
kumarchidepudi <venkat.chidepudi@gmail.com>
2024-07-27 15:26:36 +0530

    Initial commit

---
+++ src/main/java/com/kumar/RetryMechanismApplication.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryMechanismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetryMechanismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kumar/RetryMechanismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryMechanismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
